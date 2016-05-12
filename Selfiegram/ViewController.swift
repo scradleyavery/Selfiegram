f4ca2b7dedf42e5cb117b132c8400945ce10f09d
scradleyavery <bradavery@brads-macbook-pro.local>
2016-05-12 19:01:39 -0400

    Initial Commit

---
+++ Selfiegram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfiegram
+//
+//  Created by Brad Avery on 2016-05-12.
+//  Copyright © 2016 Brad Avery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
